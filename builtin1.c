1defc8aa2d519192e2f11cd90edde0024c7a53ee
Leephakoe <leephakoe@gmail.com>
2022-11-17 14:45:43 -0800

    README.md

--- builtin1.c
+++ builtin1.c
@@ -2,10 +2,10 @@
 
 /**
  * _myhistory - displays the history list, one command by line, preceded
- * with line numbers, starting at 0.
+ *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ *        constant function prototype.
+ *  Return: Always 0
  */
 int _myhistory(info_t *info)
 {
@@ -31,7 +31,7 @@ int unset_alias(info_t *info, char *str)
 	c = *p;
 	*p = 0;
 	ret = delete_node_at_index(&(info->alias),
-			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 	*p = c;
 	return (ret);
 }
@@ -44,17 +44,18 @@ int unset_alias(info_t *info, char *str)
  * Return: Always 0 on success, 1 on error
  */
 int set_alias(info_t *info, char *str)
+{
 	char *p;
 
 	p = _strchr(str, '=');
 	if (!p)
-	return (1);
+		return (1);
 	if (!*++p)
-	return (unset_alias(info, str));
+		return (unset_alias(info, str));
 
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
-	}
+}
 
 /**
  * print_alias - prints an alias string
@@ -82,8 +83,8 @@ int print_alias(list_t *node)
 /**
  * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _myalias(info_t *info)
 {
